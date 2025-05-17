8688371f8b4b6c5dff51d5e3b72cec3360dd62b0
Julius Hetzel <j.fr.hetzel@gmail.com>
2025-05-17 14:04:12 +0200

    feat: flatten all dives by active chains

--- src/lead_control/utils.py
+++ src/lead_control/utils.py
@@ -6,11 +6,18 @@ def flatten_active_device_chain(track_or_chain):
     devices = []
     chain_devices = track_or_chain.devices if liveobj_valid(track_or_chain) else []
     for device in chain_devices:
-        devices.append(device)
-        devices.extend(flatten_active_device_chain(active_chain(device)))
-
+        if device.is_active:
+            devices.append(device)
+            if is_device_rack(device):
+                child_chains = device.chains or []
+                devices.extend(flatten_chains(child_chains))
     return devices
 
-def active_chain(device):
-    if is_device_rack(device) and device.view.is_showing_chain_devices and device.is_active:
-        return device.view.selected_chain
+
+def flatten_chains(chains):
+    devices = []
+    for chain in chains:
+        devices.extend(flatten_active_device_chain(chain))
+    return devices
+
+
